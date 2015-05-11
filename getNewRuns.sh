447db15dbb38a618de89e6e4387d99860b69505e
Pamela <pamc@hep.wisc.edu>
2015-05-11 14:25:43 +0200

    Update to use DAS query, different date format.

--- getNewRuns.sh
+++ getNewRuns.sh
@@ -2,7 +2,7 @@
 
 if [ -e newRuns.txt ]
 then
-  rm newRuns.txt
+  mv newRuns.txt oldRuns.txt
 fi
 
 if [ -e diff.txt ]
@@ -15,7 +15,7 @@ do
 
 ss="${i} days ago"
 
-DATE=`date --date="$ss" +%F`
+DATE=`date --date="$ss" +%Y%m%d`
 
 echo $DATE
 
@@ -23,7 +23,10 @@ NPROC=$(($NPROC+1))
 
 #dbs search --query="find run, run.createdate where run.numlss >= 20 and run.createdate >= $DATE and file like */MinimumBias/RAW/v1*" > newRuns.txt
 #dbsql "find run, run.createdate where run.numlss > = 20 and run.createdate = 2012-05-14 and file like *Run2012B/MultiJet/RAW/v1*" >> newRuns.txt &
-dbsql "find run, run.createdate where run.createdate = $DATE and file like *Commissioning2014/Cosmics/RAW/v3*" >> newRuns.txt &
+#dbsql "find run, run.createdate where run.createdate = $DATE and file like *Commissioning2014/Cosmics/RAW/v3*" >> newRuns.txt &
+
+das_client.py --query="run date = ${DATE} | grep run.run_number, run.start_time,
+ run.nlumis>10" >> newRuns.txt
 
 if [ "$NPROC" -ge 5 ]; then
         wait
@@ -32,13 +35,12 @@ fi
 
 done
 
-grep GMT newRuns.txt | sort -r > newerRuns.txt
+grep " 201" newRuns.txt | sort -r > newerRuns.txt
 
 /bin/mv newerRuns.txt newRuns.txt
 
 if [ -e oldRuns.txt ]
 then
   diff oldRuns.txt newRuns.txt | sed -n '/^>/ p' | sed 's/^.\{2\}//g' > diff.txt
+  /bin/mv diff.txt newRuns.txt
 fi
-
-mv newRuns.txt oldRuns.txt
